6015c4e9229a785271db6cd134748890dc071a39
guochaoshun <657257897@qq.com>
2020-09-23 19:15:33 +0800

    完成堆排序

--- Heap/Heap/Heap.m
+++ Heap/Heap/Heap.m
@@ -27,15 +27,28 @@
 
 /// 把数组放入到堆中,并构建一次
 - (instancetype)initWithArray:(int [])array Size:(int)size heapType:(HeapType)type {
-    self = [self initWithSize:size+1];
+    self = [super init];
     if (self) {
         
-        heap = (int * ) malloc(size * sizeof(int));
+        _size = size;
+        if (size<100) {
+            _size = 100;
+        }
+        
+        heap = (int * ) malloc(_size * sizeof(int));
         // 把数组中的数据copy到堆上
-        memcpy(heap, array, size);
+        memcpy(heap, array, (size * sizeof(int)));
         indexNum = size;
         _type = type;
-        [self rebuildHeap];
+        
+        // 初始化堆, 假设有一种最差的情况,初始化的时候堆顶为最小元素, 此时需要把最小元素放到叶子节点上,每次循环都会把最小元素下移一层,需要循环的次数为树的深度
+        int leftNum = size;
+        while (leftNum>0) {
+            // 把最小的元素向下移动一层,循环次数为完全二叉树的深度
+            [self initHeap];
+            leftNum /=2;
+        }
+        
     }
     return self;
 }
@@ -43,7 +56,6 @@
 - (instancetype)init{
     return [self initWithSize:100];
 }
-
 
 - (instancetype)initWithSize:(int)size {
     self = [super init];
@@ -83,7 +95,7 @@
         _size *= 2;
         int * newHeap = (int * ) malloc(_size * sizeof(int));
         // 把原来数组中的数据copy到新的数组上
-        memcpy(newHeap, heap, indexNum+1);
+        memcpy(newHeap, heap, (indexNum-1)*sizeof(int));
 //        for (int i = 0; i<indexNum; i++) {
 //
 //            newHeap[i] = heap[i];
@@ -112,7 +124,7 @@
     }
     
      // 如果堆中数字已经很少了,需要收回部分内存, 容量减少1倍, 把旧数据移动到新数组中
-    if (indexNum<_size/4&&_size>100) {
+    if (indexNum<_size/4&&_size>=100) {
         _size /= 2;
         
         int * newHeap = (int * ) malloc(_size * sizeof(int));
@@ -135,14 +147,14 @@
 }
 
 
-/// 重新构造大顶堆 : 比较并调整使当前节点的值大于左右叶子
-- (void)rebuildHeap {
+/// 重新构造堆 : 比较并调整使当前节点的值与左右叶子
+- (void)initHeap {
     // 从第一个有叶子的节点开始
     for (int i = indexNum/2-1; i>=0; i--) {
         int left = 2*i+1;
         int right = 2*i+2;
-        // 保证 i>左叶子,
-        if (heap[i]<heap[left]) {
+        //  (大顶堆&&i<左叶子,进行交换) || (小顶堆&&i>左叶子,进行交换)
+        if ( (_type==HeapBigTop && heap[i]<heap[left]) || (_type==HeapSmallTop && heap[i]>heap[left]) ) {
             int temp = heap[i];
             heap[i] = heap[left];
             heap[left] = temp;
@@ -152,8 +164,8 @@
             continue;
         }
         
-        // 保证i>右叶子
-        if (heap[i]<heap[right]) {
+        //  (大顶堆&&i<右叶子,进行交换) || (小顶堆&&i>右叶子,进行交换)
+        if ( (_type==HeapBigTop && heap[i]<heap[right]) || (_type==HeapSmallTop && heap[i]>heap[right]) ) {
             int temp = heap[i];
             heap[i] = heap[right];
             heap[right] = temp;
@@ -161,6 +173,65 @@
         
     }
 }
+
+
+/// 重构堆, 把堆顶元素放到堆中的合适位置
+- (void)rebuildHeap {
+
+    // 堆在前面已经初始化完成了,但是进行了add/pop操作,需要重新构建堆,堆顶元素是一个比较小的值,需要重新再堆中找合适的位置
+    // 从第一个根节点开始构建,然后构建左子树或者右子树,
+    // 比如堆顶为3, 左节点为5,右节点为7, 此时把堆顶和右节点交换, 重构右子树,左子树不需要处理, 这样时间复杂度度就是O(logN)
+    int nextIndex = 0; // 下一个要重构的节点
+    // 最后一个带叶子的节点endIndex/2-1,
+    for (int i = 0; i<=indexNum/2-1; i=nextIndex) {
+        int left = 2*i+1;
+        int right = 2*i+2;
+        
+        // 找出左右节点中需要交换的那个,大顶堆,找大节点; 小顶堆,找小节点
+        nextIndex = left;
+        // 大顶堆 && 有右节点 && 右节点>左节点
+        if (_type==HeapBigTop && right<indexNum && heap[left] < heap[right] ) {
+            nextIndex = right;
+        }
+        // 小顶堆 && 有右节点 && 右节点<左节点
+        if (_type == HeapSmallTop && right<indexNum && heap[left] > heap[right] ) {
+            nextIndex = right;
+        }
+        // 现在nextIndex就是是否要交换的节点了
+        
+        
+        if (_type==HeapBigTop) {
+            
+            if (heap[i]<heap[nextIndex]) {
+                int temp = heap[i];
+                heap[i] = heap[nextIndex];
+                heap[nextIndex] = temp;
+            } else {
+                // 说明 已经满足条件了, 节点>左节点 && 节点>右节点, 堆顶元素找到了合适的位置, 结束循环
+                break;
+            }
+            
+        }
+        
+        if (_type==HeapSmallTop) {
+            
+            if (heap[i]>heap[nextIndex]) {
+                int temp = heap[i];
+                heap[i] = heap[nextIndex];
+                heap[nextIndex] = temp;
+            } else {
+                // 说明 小顶堆已经满足条件了, 节点<左节点 && 节点<右节点, 堆顶元素找到了合适的位置, 结束循环
+                break;
+            }
+            
+        }
+        
+        
+    }
+    
+    
+}
+
 
 - (NSString *)description {
     
--- Heap/Heap/ViewController.m
+++ Heap/Heap/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Heap.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self heapSort];
+    
+    
 }
+
+- (void)heapSort {
+    /*
+     堆排序
+     */
+    int num = 20;
+    int a[num];
+    
+    for (int i = 0; i<num; i++) {
+        a[i] = arc4random()%(num*10);
+        printf("%d -> ",a[i]);
+    }
+    printf("初始化完成\n");
+    // 开始排序,小顶堆
+//    Heap * heap = [[Heap alloc] initWithArray:a Size:num heapType:HeapSmallTop];
+//    for (int i = 0; i<num; i++) {
+//         a[i] = heap.popTopValue;
+//     }
+    
+    // 开始排序,大顶堆
+    Heap * heap = [[Heap alloc] initWithArray:a Size:num heapType:HeapBigTop];
+    for (int i = num-1; i>=0; i--) {
+         a[i] = heap.popTopValue;
+     }
+    
+    for (int i = 0; i<num; i++) {
+        printf("%d -> ",a[i]);
+    }
+    printf("排序完成\n");
+    for (int i = 0; i<num-1; i++) {
+        if (a[i]>a[i+1]) {
+            NSLog(@"出错了 %d",i);
+            NSAssert(NO, @"排序不对");
+        }
+        
+    }
+    
+}
+
 
 
 @end
